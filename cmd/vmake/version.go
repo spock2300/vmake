56eb22605fbf596d86e4ac1ee65c8992f86225d1
spock <2278089202@qq.com>
2026-02-16 15:39:42 +0800

    feat: 自动生成 Git commit 和 build date

--- cmd/vmake/version.go
+++ cmd/vmake/version.go
@@ -7,19 +7,14 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	GitCommit = "unknown"
-	BuildDate = "unknown"
-)
-
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version information",
 	Long:  `Display the version, git commit, and build date of vmake.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("vmake %s\n", version.Version)
-		fmt.Printf("  Git commit: %s\n", GitCommit)
-		fmt.Printf("  Build date: %s\n", BuildDate)
+		fmt.Printf("  Git commit: %s\n", version.GitCommit)
+		fmt.Printf("  Build date: %s\n", version.BuildDate)
 	},
 }
 
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,13 +2,48 @@ package version
 
 import (
 	"runtime/debug"
+	"strings"
 )
 
-var Version = func() string {
-	if info, ok := debug.ReadBuildInfo(); ok {
-		if info.Main.Version != "" && info.Main.Version != "(devel)" {
-			return info.Main.Version
+var (
+	Version   string
+	GitCommit string
+	BuildDate string
+)
+
+func init() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		Version = "dev"
+		GitCommit = "unknown"
+		BuildDate = "unknown"
+		return
+	}
+
+	if info.Main.Version != "" && info.Main.Version != "(devel)" {
+		Version = info.Main.Version
+	} else {
+		Version = "dev"
+	}
+
+	for _, s := range info.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			if len(s.Value) >= 8 {
+				GitCommit = s.Value[:8]
+			} else {
+				GitCommit = s.Value
+			}
+		case "vcs.time":
+			BuildDate = strings.Replace(s.Value, "T", " ", 1)
+			BuildDate = strings.TrimSuffix(BuildDate, "Z")
 		}
 	}
-	return "dev"
-}()
+
+	if GitCommit == "" {
+		GitCommit = "unknown"
+	}
+	if BuildDate == "" {
+		BuildDate = "unknown"
+	}
+}
