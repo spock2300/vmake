ba4767fc44f34123145270a0061450b0c1f4f6c0
spock <2278089202@qq.com>
2026-02-16 15:04:07 +0800

    feat: 添加 update 命令支持 vmake 自更新

---
+++ cmd/vmake/update.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/spf13/cobra"
+)
+
+var updateVersion string
+
+var updateCmd = &cobra.Command{
+	Use:   "update [version]",
+	Short: "Update vmake to latest or specified version",
+	Long: `Update vmake using go install.
+
+Examples:
+  vmake update          Update to latest version
+  vmake update v1.2.3   Update to specific version`,
+	Run: runUpdate,
+}
+
+func init() {
+	RootCmd.AddCommand(updateCmd)
+}
+
+func runUpdate(cmd *cobra.Command, args []string) {
+	version := "latest"
+	if len(args) > 0 {
+		version = args[0]
+	}
+
+	fmt.Printf("Current version: %s\n", Version)
+	fmt.Printf("Installing vmake@%s...\n", version)
+
+	pkg := fmt.Sprintf("gitee.com/spock2300/vmake/cmd/vmake@%s", version)
+
+	goCmd, err := exec.LookPath("go")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: go command not found: %v\n", err)
+		os.Exit(1)
+	}
+
+	installCmd := exec.Command(goCmd, "install", pkg)
+	installCmd.Stdout = os.Stdout
+	installCmd.Stderr = os.Stderr
+
+	if err := installCmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: update failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Update to %s completed!\n", version)
+}
