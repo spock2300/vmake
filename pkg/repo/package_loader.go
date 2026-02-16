c5c8de2b3267d47ba4bd1289bc68380fc728f37c
spock <2278089202@qq.com>
2026-02-16 15:31:52 +0800

    fix: update package_loader to use go modules

--- pkg/repo/package_loader.go
+++ pkg/repo/package_loader.go
@@ -6,9 +6,9 @@ import (
 	"os/exec"
 	"path/filepath"
 	"plugin"
-	"strings"
 
 	"gitee.com/spock2300/vmake/pkg/api"
+	"gitee.com/spock2300/vmake/pkg/version"
 )
 
 type PackageLoader struct {
@@ -45,12 +45,12 @@ func (l *PackageLoader) Load(pkgPath string) (*api.Package, error) {
 func (l *PackageLoader) compile(pkgPath string) (string, error) {
 	vmakeDir := os.Getenv("VMAKE_DIR")
 	if vmakeDir != "" {
-		return l.compileWithGoMod(pkgPath, vmakeDir)
+		return l.compileWithGoModReplace(pkgPath, vmakeDir)
 	}
-	return l.compileWithGopath(pkgPath)
+	return l.compileWithGoModVersion(pkgPath)
 }
 
-func (l *PackageLoader) compileWithGoMod(pkgPath, vmakeDir string) (string, error) {
+func (l *PackageLoader) compileWithGoModReplace(pkgPath, vmakeDir string) (string, error) {
 	if err := os.MkdirAll(l.goPath, 0755); err != nil {
 		return "", err
 	}
@@ -102,48 +102,52 @@ replace gitee.com/spock2300/vmake => %s
 	return pluginPath, nil
 }
 
-func (l *PackageLoader) compileWithGopath(pkgPath string) (string, error) {
-	vmakePath, err := l.getVMakePath()
-	if err != nil {
-		return "", err
-	}
-
-	userGopath, err := l.getUserGopath()
-	if err != nil {
-		return "", err
-	}
-
-	srcDir := filepath.Join(userGopath, "src", "gitee.com", "spock2300")
-	if err := os.MkdirAll(srcDir, 0755); err != nil {
-		return "", err
-	}
-
-	linkPath := filepath.Join(srcDir, "vmake")
-	linkTarget, _ := os.Readlink(linkPath)
-	if linkTarget != vmakePath {
-		os.Remove(linkPath)
-		if err := os.Symlink(vmakePath, linkPath); err != nil {
-			return "", err
-		}
+func (l *PackageLoader) compileWithGoModVersion(pkgPath string) (string, error) {
+	vmakeVersion := version.Version
+	if vmakeVersion == "dev" {
+		vmakeVersion = "latest"
 	}
 
 	if err := os.MkdirAll(l.goPath, 0755); err != nil {
 		return "", err
 	}
 
-	hash := hashPath(pkgPath + vmakePath)
+	hash := hashPath(pkgPath + vmakeVersion)
 	buildDir := filepath.Join(l.goPath, hash)
 	if err := os.MkdirAll(buildDir, 0755); err != nil {
 		return "", err
 	}
 
+	srcContent, err := os.ReadFile(pkgPath)
+	if err != nil {
+		return "", fmt.Errorf("failed to read package source: %w", err)
+	}
+
+	if err := os.WriteFile(filepath.Join(buildDir, "package.go"), srcContent, 0644); err != nil {
+		return "", fmt.Errorf("failed to copy package source: %w", err)
+	}
+
+	goModContent := fmt.Sprintf(`module vmake_package
+
+go 1.22
+
+require gitee.com/spock2300/vmake %s
+`, vmakeVersion)
+	if err := os.WriteFile(filepath.Join(buildDir, "go.mod"), []byte(goModContent), 0644); err != nil {
+		return "", fmt.Errorf("failed to create go.mod: %w", err)
+	}
+
+	tidyCmd := exec.Command("go", "mod", "tidy")
+	tidyCmd.Dir = buildDir
+	tidyCmd.Env = append(os.Environ(), "GO111MODULE=on")
+	if tidyOutput, err := tidyCmd.CombinedOutput(); err != nil {
+		return "", fmt.Errorf("go mod tidy error: %s", string(tidyOutput))
+	}
+
 	pluginPath := filepath.Join(buildDir, "plugin.so")
-	cmd := exec.Command("go", "build", "-buildmode=plugin", "-o", "plugin.so", pkgPath)
+	cmd := exec.Command("go", "build", "-buildmode=plugin", "-o", "plugin.so", ".")
 	cmd.Dir = buildDir
-	cmd.Env = append(os.Environ(),
-		"GO111MODULE=off",
-		"GOPATH="+userGopath,
-	)
+	cmd.Env = append(os.Environ(), "GO111MODULE=on")
 
 	output, err := cmd.CombinedOutput()
 	if err != nil {
@@ -151,34 +155,6 @@ func (l *PackageLoader) compileWithGopath(pkgPath string) (string, error) {
 	}
 
 	return pluginPath, nil
-}
-
-func (l *PackageLoader) getVMakePath() (string, error) {
-	if l.vmakeDir != "" {
-		return l.vmakeDir, nil
-	}
-	cmd := exec.Command("go", "list", "-m", "-f", "{{.Dir}}", "gitee.com/spock2300/vmake")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("vmake module not found (run 'go install gitee.com/spock2300/vmake/cmd/vmake@latest' or set VMAKE_DIR): %w", err)
-	}
-	return strings.TrimSpace(string(output)), nil
-}
-
-func (l *PackageLoader) getUserGopath() (string, error) {
-	cmd := exec.Command("go", "env", "GOPATH")
-	output, err := cmd.Output()
-	if err != nil {
-		return "", fmt.Errorf("failed to get GOPATH: %w", err)
-	}
-	gopath := strings.TrimSpace(string(output))
-	if gopath == "" {
-		return "", fmt.Errorf("GOPATH is not set")
-	}
-	if idx := strings.Index(gopath, string(os.PathListSeparator)); idx > 0 {
-		gopath = gopath[:idx]
-	}
-	return gopath, nil
 }
 
 func (l *PackageLoader) loadPlugin(pluginPath string) (*api.Package, error) {
