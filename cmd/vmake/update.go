2b60ff3f646fd29663a8f94387d05ce229d8ebef
spock <2278089202@qq.com>
2026-04-25 17:11:35 +0800

    feat: update command resolves latest version via semver instead of @latest pseudo-version

--- cmd/vmake/update.go
+++ cmd/vmake/update.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	iexec "gitee.com/spock2300/vmake/internal/exec"
+	"gitee.com/spock2300/vmake/pkg/api"
 	"gitee.com/spock2300/vmake/pkg/version"
 	"github.com/spf13/cobra"
 )
+
+const modulePath = "gitee.com/spock2300/vmake/cmd/vmake"
 
 var updateCmd = &cobra.Command{
 	Use:   "update [version]",
@@ -14,7 +18,7 @@ var updateCmd = &cobra.Command{
 	Long: `Update vmake using go install.
 
 Examples:
-  vmake update          Update to latest version
+  vmake update          Update to latest stable version
   vmake update v1.2.3   Update to specific version`,
 	Run: runUpdate,
 }
@@ -24,20 +28,50 @@ func init() {
 }
 
 func runUpdate(cmd *cobra.Command, args []string) {
-	targetVer := "latest"
-	if len(args) > 0 {
-		targetVer = args[0]
-	}
-
 	fmt.Printf("Current version: %s\n", version.Version)
-	fmt.Printf("Installing vmake@%s...\n", targetVer)
-
-	pkg := fmt.Sprintf("gitee.com/spock2300/vmake/cmd/vmake@%s", targetVer)
 
 	goCmd, err := iexec.LookPath("go")
 	fatalErr(err)
 
-	fatalErr(iexec.RunToStdout("", goCmd, "install", pkg))
+	targetVer := ""
+	if len(args) > 0 {
+		targetVer = args[0]
+	} else {
+		versions, err := listAvailableVersions(goCmd)
+		fatalErr(err)
+		targetVer = pickLatestStable(versions)
+		fmt.Printf("Available versions: %s\n", strings.Join(versions, " "))
+		fmt.Printf("Selected latest: %s\n", targetVer)
+	}
 
+	fmt.Printf("Installing vmake@%s...\n", targetVer)
+	fatalErr(iexec.RunToStdout("", goCmd, "install", fmt.Sprintf("%s@%s", modulePath, targetVer)))
 	fmt.Printf("Update to %s completed!\n", targetVer)
+}
+
+func listAvailableVersions(goCmd string) ([]string, error) {
+	output, err := iexec.RunWithOptions(goCmd, []string{"list", "-m", "-versions", "gitee.com/spock2300/vmake"}, iexec.RunOptions{Quiet: true})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list versions: %w", err)
+	}
+	parts := strings.Fields(iexec.TrimOutput(output))
+	if len(parts) <= 1 {
+		return nil, fmt.Errorf("no published versions found")
+	}
+	return parts[1:], nil
+}
+
+func pickLatestStable(versions []string) string {
+	var stable []string
+	for _, v := range versions {
+		pv, ok := api.ParseVersion(v)
+		if ok && pv.Pre == "" {
+			stable = append(stable, v)
+		}
+	}
+	result, ok := api.MatchVersion(stable, ">=0.0.0")
+	if !ok {
+		fatalMsg("no stable version available")
+	}
+	return result
 }
